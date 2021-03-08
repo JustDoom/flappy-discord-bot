f828289041e0d8536fd57916d30aad0b1fdacdb6
JustDoom <61824552+JustDoom@users.noreply.github.com>
2021-03-08 14:30:41 +1000

    edit

--- commands/addserverstodatabase.js
+++ commands/addserverstodatabase.js
@@ -1,13 +1,18 @@
 module.exports = {
     name: 'addservertodatabase',
     execute(message, args, Discord, db, client, con) {
+        console.log('1');
         client.guilds.cache.forEach(server => {
-            con.query(`SELECT * FROM data WHERE guildID=${server.id}`, function (err, result, rows) {
+            console.log('2');
+            con.query(`SELECT * FROM data WHERE guildID = ${server.id}`, function (err, result, rows) {
+                console.log(server.id)
                 if (err) {
                     return err;
-                } else if (!result) {
-                    con.query(`INSERT INTO data (guildID, prefix, commands, messages) VALUES ('${message.guild.id}', '?', 0, 0)`), (err2, result) => {
+                } else if (!result.length) {
+                    console.log('3');
+                    con.query(`INSERT INTO data (guildID, prefix, commands, messages) VALUES (${server.id}, "?", 0, 0)`), (err2, result) => {
                         if (err2) throw err2;
+                        console.log('4');
                         message.channel.send(`${server.name} has been added`);
                     };
                 }
